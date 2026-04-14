e6537cb30ca6caf7d44ef52e6018ec111e7f6009
jheferson Vieira <jhefersonlv@hotmail.com>
2026-04-14 19:01:34 -0300

    modified:   assets/js/firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -4,16 +4,16 @@
    ═══════════════════════════════════════════════════════════════ */
 
 const FIREBASE_CONFIG = {
-  apiKey:            "SUA-API-KEY",
-  authDomain:        "seu-projeto.firebaseapp.com",
-  projectId:         "seu-projeto-id",
-  storageBucket:     "seu-projeto.appspot.com",
-  messagingSenderId: "000000000000",
-  appId:             "1:000000000000:web:000000000000000000000000"
+  apiKey: "AIzaSyBU92Mx34C-qTuV_jCsK4sQ_MbK25LNg4I",
+  authDomain: "kamilly-beauty-art.firebaseapp.com",
+  projectId: "kamilly-beauty-art",
+  storageBucket: "kamilly-beauty-art.firebasestorage.app",
+  messagingSenderId: "806664103449",
+  appId: "1:806664103449:web:5e9c1cfb671ea01fc36c88"
 };
 
 firebase.initializeApp(FIREBASE_CONFIG);
 
 /* Exporta como globais para main.js e admin.js */
-const db   = firebase.firestore();
+const db = firebase.firestore();
 const auth = firebase.auth();
